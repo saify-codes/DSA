ad41999fe1b7d5c695b20bcc2a55992adfac455b
saify <sarfaraz.symits@gmail.com>
2025-01-01 20:33:43 +0500

    practice

--- practice/linkedlist.js
+++ practice/linkedlist.js
@@ -1,121 +1,144 @@
-class Node{
-    constructor(data){
-        this.data = data
-        this.next = null
-    }
+class Node {
+  constructor(data) {
+    this.data = data;
+    this.next = null;
+  }
 }
 
+class LinkedList {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+    this.length = null;
+  }
 
-class LinkedList{
-    constructor(){
-        this.head = null
-        this.tail = null
-        this.length = null
+  empty() {
+    return this.head === null && this.tail === null;
+  }
+
+  insertHead(data) {
+    const node = new Node(data);
+
+    if (this.empty()) {
+      this.head = node;
+      this.tail = node;
+    } else {
+      node.next = this.head;
+      this.head = node;
     }
 
-    empty(){
-        return this.head === null && this.tail === null
+    this.length++;
+  }
+  insertTail(data) {
+    const node = new Node(data);
+
+    if (this.empty()) {
+      this.head = node;
+      this.tail = node;
+    } else {
+      this.tail.next = node;
+      this.tail = node;
     }
 
-    insertHead(data){
-        const node = new Node(data)
-
-        if (this.empty()) {
-            this.head = node
-            this.tail = node
-        }else{
-
-            node.next = this.head
-            this.head = node
-
-        }
-
-        this.length++
+    this.length++;
+  }
+  insertMiddle(data, index) {
+    if (index < 0 || index > this.length - 1) {
+      throw Error("index out of bound");
     }
-    insertTail(data){
-        const node = new Node(data)
 
-        if (this.empty()) {
-            this.head = node
-            this.tail = node
-        }else{
-
-            this.tail.next = node
-            this.tail = node
-
-        }
-
-        this.length++
+    if (index === 0) {
+      this.insertHead(data);
+      return;
     }
-    insertMiddle(data, index){
-        if (index < 0 || index > this.length - 1) {
-            throw Error('index out of bound')
-        }
 
-        if (index === 0) {
-            this.insertHead(data)
-            return
-        }
+    const node = new Node(data);
+    let current = this.head;
 
-        const node    = new Node(data)
-        let current = this.head
-
-        // logi 1
-        for(let i = 1; i < index && current; i++){
-            current = current.next
-        }
-
-        
-        // logic 2
-        // while (index > 0 && tmp) {
-        //     tmp.next = tmp
-        //     index--
-        // }
-
-        if (!current) {
-            throw Error('index out of bound')
-            
-        }
-        
-        node.next = current.next
-        current.next = node
-
-        this.length++
-
+    // logi 1
+    for (let i = 1; i < index && current; i++) {
+      current = current.next;
     }
-    deleteHead(){
 
-        if (this.empty()) {
-            return
-        }
+    // logic 2
+    // while (index > 0 && tmp) {
+    //     tmp.next = tmp
+    //     index--
+    // }
 
-        if (this.head === this.tail) {
-            this.head = null
-            this.tail = null
-        }else{
-            
-            this.head = this.head.next
+    if (!current) {
+      throw Error("index out of bound");
+    }
 
-        }
+    node.next = current.next;
+    current.next = node;
 
-        this.length--
+    this.length++;
+  }
+  deleteHead() {
+    // If the list is empty, return immediately
+    if (this.empty()) {
+      return;
+    }
+
+    // If there is only one node in the list
+    if (this.head === this.tail) {
+      this.head = null;
+      this.tail = null;
+    } else {
+      // Store reference to current head
+      const tmp = this.head;
+      // Move head to the next node
+      this.head = this.head.next;
+      // Clean up old head
+      tmp.next = null;
+    }
+
+    // Decrement the length
+    this.length--;
+  }
+  deleteTail() {
+    // If the list is empty, return immediately
+    if (this.empty()) {
+      return;
+    }
+
+    // If there is only one node in the list
+    if (this.head === this.tail) {
+      this.head = null;
+      this.tail = null;
+    } else {
+
+      let current = this.head
+
+      while(current.next != this.tail){
+        current = current.next
+      }
+
+      current.next = null
+      this.tail = current
 
     }
-    traverse(){
-        let tmp = this.head
 
-        while(tmp){
-            console.log(tmp.data);
-            tmp = tmp.next
-        }
+    // Decrement the length
+    this.length--;
+  }
+
+  traverse() {
+    let tmp = this.head;
+
+    while (tmp) {
+      console.log(tmp.data);
+      tmp = tmp.next;
     }
+  }
 }
 
+const list = new LinkedList();
 
-const list = new LinkedList()
+list.insertTail(1);
+list.insertTail(2);
+list.insertTail(3);
 
-list.insertTail(1)
-list.insertTail(2)
-list.insertTail(3)
 
 console.log(list.traverse());
